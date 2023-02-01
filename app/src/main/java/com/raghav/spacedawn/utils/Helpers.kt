496031b1424b83a16b8f9c27ae75b5bd8867bc5f
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-02-01 15:19:20 +0530

    add extension function to check network connectivity

--- app/src/main/java/com/raghav/spacedawn/utils/Helpers.kt
+++ app/src/main/java/com/raghav/spacedawn/utils/Helpers.kt
@@ -1,14 +1,18 @@
 package com.raghav.spacedawn.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-import java.util.TimeZone
+import java.util.*
 
 class Helpers {
 
     companion object {
-        fun String.toDate(dateFormat: String, timeZone: TimeZone = TimeZone.getTimeZone("UTC")): Date {
+        fun String.toDate(
+            dateFormat: String,
+            timeZone: TimeZone = TimeZone.getTimeZone("UTC")
+        ): Date {
             val parser = SimpleDateFormat(dateFormat, Locale.getDefault())
             parser.timeZone = timeZone
             return parser.parse(this)
@@ -18,6 +22,32 @@ class Helpers {
             val formatter = SimpleDateFormat(dateFormat, Locale.getDefault())
             formatter.timeZone = timeZone
             return formatter.format(this)
+        }
+
+        fun Context.isConnectedToNetwork(): Boolean {
+            // register activity with the connectivity manager service
+            val connectivityManager =
+                this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+            // Returns a Network object corresponding to
+            // the currently active default data network.
+            val network = connectivityManager.activeNetwork ?: return false
+
+            // Representation of the capabilities of an active network.
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when {
+                // Indicates this network uses a Wi-Fi transport,
+                // or WiFi has network connectivity
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+
+                // Indicates this network uses a Cellular transport. or
+                // Cellular has network connectivity
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+                // else return false
+                else -> false
+            }
         }
     }
 }
