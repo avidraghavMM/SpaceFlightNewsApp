f9dca17b4561cf099ddcb9b71010f1f7013b932c
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-02-08 14:14:36 +0530

    remove unused code

--- app/src/main/java/com/raghav/spacedawn/utils/AppApplication.kt
+++ app/src/main/java/com/raghav/spacedawn/utils/AppApplication.kt
@@ -1,25 +1,8 @@
 package com.raghav.spacedawn.utils
 
 import android.app.Application
-import com.raghav.spacedawn.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AppApplication : Application() {
-
-    companion object {
-        lateinit var INSTANCE: AppApplication
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        INSTANCE = this
-    }
-
-    fun getLaunchLibraryBaseUrl(): String {
-        if (BuildConfig.DEBUG) {
-            return Constants.DEBUG_BASE_URL_LAUNCHLIBRARY
-        }
-        return Constants.BASE_URL_LAUNCHLIBRARY
-    }
 }
