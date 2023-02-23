26c3ece347ca2301848c9fe1fcc38a8dee8e585a
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-02-23 12:06:36 +0530

    Add ComposeView in SearchArticleFragment.kt

--- app/src/main/java/com/raghav/spacedawn/ui/viewmodels/SearchArticleFragmentVM.kt
+++ app/src/main/java/com/raghav/spacedawn/ui/viewmodels/SearchArticleFragmentVM.kt
@@ -1,55 +1,45 @@
 package com.raghav.spacedawn.ui.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.raghav.spacedawn.models.spaceflightapi.ArticlesResponseItem
 import com.raghav.spacedawn.repository.AppRepository
-import com.raghav.spacedawn.utils.Constants
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-@OptIn(FlowPreview::class)
 @HiltViewModel
 class SearchArticleFragmentVM @Inject constructor(
     private val repository: AppRepository
 ) : ViewModel() {
 
-    private var _searchArticlesList: MutableStateFlow<PagingData<ArticlesResponseItem>?> =
-        MutableStateFlow(null)
-    val searchArticlesList: StateFlow<PagingData<ArticlesResponseItem>?> get() = _searchArticlesList
+    private var _searchArticlesList: Flow<PagingData<ArticlesResponseItem>> =
+        emptyFlow()
+    val searchArticlesList: Flow<PagingData<ArticlesResponseItem>> get() = _searchArticlesList
 
-    var query = MutableStateFlow("")
+    var searchQuery by mutableStateOf("")
+        private set
 
     init {
-        viewModelScope.launch {
-            query
-                .debounce(Constants.SEARCH_DELAY_TIME)
-                .collect {
-                    getSearchArticleList(it)
-                }
-        }
+        getSearchArticleList(searchQuery)
     }
 
-    private fun getSearchArticleList(searchQuery: String) {
+    fun getSearchArticleList(query: String) {
+        searchQuery = query
         viewModelScope.launch {
             repository.searchArticle(searchQuery)
                 .cachedIn(this)
                 .collect {
-                    _searchArticlesList.emit(it)
+                    _searchArticlesList = flowOf(it)
                 }
-        }
-    }
-
-    fun setSearchQuery(searchQuery: String) {
-        viewModelScope.launch {
-            query.emit(searchQuery)
         }
     }
 }
