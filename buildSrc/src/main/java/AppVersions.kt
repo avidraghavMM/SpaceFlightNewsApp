38778fd41da7e1a175ea77a6bf64d58049901e69
Raghav Aggarwal <raghavaggarwal776@gmail.com>
2023-03-14 12:35:05 +0530

    add buildSrc

---
+++ buildSrc/src/main/java/AppVersions.kt
@@ -0,0 +1,14 @@
+object AppVersions {
+    private const val versionMajor = 1
+    private const val versionMinor = 0
+    private const val versionPatch = 0
+
+    const val versionCode =1
+    const val versionName = "1.0"
+
+    const val COMPILE_SDK = 33
+     const val BUILD_TOOL_VERSION = "30.0.3"
+    const val MIN_SDK = 26
+    const val TARGET_SDK = 33
+    const val APPLICATION_ID = "com.raghav.spacedawn"
+}
